804380f80f2f38f5a6f26a0c5311813ec8452155
AngelSantos1 <angelsantoscd@gmail.com>
2021-10-06 17:08:38 -0400

    Added .gitignore file. Outlined draft of RRModels.

--- RRModels/Class1.cs
+++ RRModels/Class1.cs
@@ -2,7 +2,23 @@
 
 namespace RRModels
 {
-    public class Class1
+    public class Restaurant
     {
+         private string _city;
+        public string string City;  
+        {
+        get { return _city; }
+        set
+        {
+            //Regex will find any number inside of my string
+            if (Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+            {
+                int
+            }
+        } 
+    }
+
+    public string state {get; set}
+    public string city {get; set}
     }
 }

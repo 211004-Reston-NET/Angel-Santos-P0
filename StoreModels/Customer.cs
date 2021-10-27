166785a920ceb2f0ba86981fda3f5ca287e6a056
AngelSantos1 <angelsantoscd@gmail.com>
2021-10-27 10:17:43 -0400

    Refined Customer.cs StoreModel for SQL

--- StoreModels/Customer.cs
+++ StoreModels/Customer.cs
@@ -1,38 +1,118 @@
 using System;
 using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace StoreModels
 {
     public class Customer
     {
-        //This is a field
+        private string _firstName;
+        private string _lastName;
+        private string _streetAddress;
         private string _city;
-
-        //This is a property that uses the field called _city
+        private string _state;
+        private string _email; 
+        private string _phoneNumber;
+        
+        public string FirstName 
+        { 
+        get { return _firstName; }
+        set
+            {
+            //Regex.IsMatch() Method - checks the regular expression to match with input string
+            //Exception is thrown when Field is set with a number
+            if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {       
+                    throw new Exception("   First Name must contain only letters.  ");
+                }
+                _firstName = value;
+            } 
+        }
+        public string LastName 
+        { 
+        get { return _lastName; }
+        set
+            {
+            if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {       
+                    throw new Exception("   Last Name must contain only letters.    ");
+                }
+                _lastName = value;
+            }
+        }
+        public string StreetAddress
+        {
+            get { return _streetAddress; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {       
+                    throw new Exception
+                    (
+                        "   Street Address cannot be left empty.     "
+                    );
+                }
+                _streetAddress = value;    
+            } 
+        }
         public string City
         {
             get { return _city; }
             set 
             {
-                //Main idea - this Regex will find me any number inside of my string
-                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
-                {
-                    //Will give the user an exception whenever you try to set the city field with a number
-                    throw new Exception("City can only hold letters!");
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$" ))
+                {       
+                    throw new Exception("   City must contain only letters.    ");
                 }
                 _city = value;
             }
         }
-        public string Username {get; set; }
-        public string State { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
+        public string State 
+        {
+            get { return _state; }
+            set 
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {       
+                    throw new Exception("   State must contain only letters.    ");
+                }
+                _state = value;
+            }
+        }
+        public string Email 
+        {
+            get { return _email; }
+            set 
+            {
+                if (!MailAddress.TryCreate(value, out var mailAddress))
+                {       
+                    throw new Exception
+                    (
+                        "   Invalid Email. Please, check spelling errors or try a different email.     "
+                    );
+                }
+                _email = value;    
+            } 
+        }
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set 
+            {
+                if (!Regex.IsMatch(value, @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}$"))
+                {       
+                    throw new Exception
+                    (
+                        "   Invalid Phone Number. Please follow a (555)-(555)-(5555) format.    "
+                    );
+                }
+                _phoneNumber = value;    
+            } 
+        }
 
         public override string ToString()
         {
-            return $"Username: {Username}\nFirstName: {FirstName}\nLastName:{LastName}\nState: {State}";
+            return $"First Name: {FirstName}\nLast Name:{LastName}\nStreet Address: {StreetAddress}\nCity: {City}\nState: {State}\nEmail: {Email}\nPhone Number: {PhoneNumber}";
         }
-
     }
 }
